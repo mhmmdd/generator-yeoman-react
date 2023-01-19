ace5a9ac183da4528bc78fc7ea4bcf4c165facd5
mhmmdd <mhmmd.dd@gmail.com>
2023-01-19 22:09:13 +0300

    01 - 1. Prompts

---
+++ README.md
@@ -0,0 +1,17 @@
+```shell
+$ npm init -y
+$ npm i --save yeoman-generator
+```
+## After generators
+```shell
+$ npm link
+$ npm install -g yo
+```
+
+## Run the generator
+```shell
+$ yo yeoman-react
+```
+
+## 01 - Changes
+1. Prompts
---
+++ generators/app/index.js
@@ -0,0 +1,17 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "confirm",
+        name: "cool",
+        message: "Would you like to enable the Cool feature?"
+      }
+    ]);
+  }
+
+  writing() {
+    this.log("cool feature", this.answers.cool); // user answer `cool` used
+  }
+};
